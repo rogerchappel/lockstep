944ed11f438452d26187aa09ff4f26e6406b969b
Roger Chappel <miscanalysis@gmail.com>
2026-05-09 18:39:56 +1000

    feat: export utility helpers

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export { analyzePackage } from './drift.js';
 export { formatReport, formatMarkdown, formatTable } from './format.js';
+export { isPackageManagerAllowed, packageManagerFamily } from './package-manager.js';
+export { redactText } from './redact.js';
 export { defaultPolicy, loadPolicy, normalizePolicy, policyJson } from './policy.js';
 export { scanWorkspace } from './scanner.js';
 export type { DriftFinding, LockstepPolicy, OutputFormat, PackageRecord, ScanReport } from './types.js';
